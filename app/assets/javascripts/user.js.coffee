d29922329469980f6e7adc43011a475d03f69f08
Aaron Gordon <asgordon96@gmail.com>
2013-07-18 22:35:19 -0700

    Add user creation and validation with flash messages

--- app/assets/javascripts/user.js.coffee
+++ app/assets/javascripts/user.js.coffee
@@ -1,3 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+close_flash = -> $('.alert').slideUp(500, -> $(this).remove)
+$('a.close').bind('click', close_flash)
+window.setTimeout(close_flash, 5000)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def new
-    @new_user = User.new
   end
 
   def create
+    new_user = User.new(user_params)
+    begin
+      new_user.save!
+    rescue => error
+      message = error.message
+      flash[:"alert-error"] = message
+      redirect_to new_user_path
+    end
   end
 
   def update
@@ -11,4 +18,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+  
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation, :email)
+  end
+  
 end
