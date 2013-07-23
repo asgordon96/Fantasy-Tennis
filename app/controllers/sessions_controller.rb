cf029b1490b2f1158c2d710b4a5ef34370dbd8bf
Aaron Gordon <asgordon96@gmail.com>
2013-07-22 21:55:22 -0700

    Added login and logout for users. Started user homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
   
   def create
+    # authenticate the user
+    user = User.find_by(:email => params[:email])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      redirect_to sessions_signin_path
+      flash[:"alert-error"] = "Invalid username or password"
+    end
+
   end
   
   def signout
+    session[:user_id] = nil
+    redirect_to '/'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,21 @@ class UsersController < ApplicationController
     new_user = User.new(user_params)
     begin
       new_user.save!
+      redirect_to user_path(new_user.id)
     rescue => error
       message = error.message
       flash[:"alert-error"] = message
       redirect_to new_user_path
     end
   end
-
+  
+  def show
+    # show the user home
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+  
   def update
   end
 
