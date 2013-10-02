c9c392c7ef75c6be58fdde6b7f0c9eb63e92bc09
Aaron Gordon <asgordon96@gmail.com>
2013-10-02 16:52:09 -0700

    Test Rufus Scheduler

--- config/initializers/tasks.rb
+++ config/initializers/tasks.rb
@@ -1,6 +1,9 @@
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every '24h' do
-  Player.update_players
+# scheduler.every '24h' do
+#   Player.update_players
+# end
+scheduler.every '1s' do
+  puts "Here's the Scheduler"
 end
