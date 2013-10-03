4cb8b181e5a7c90235eb837f437a2fd7499b5401
Aaron Gordon <asgordon96@gmail.com>
2013-10-02 17:01:54 -0700

    Switch to cron syntax for rufus scheduler

--- config/initializers/tasks.rb
+++ config/initializers/tasks.rb
@@ -1,9 +1,7 @@
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.start_new
 
-# scheduler.every '24h' do
-#   Player.update_players
-# end
-scheduler.every '1s' do
-  puts "Here's the Scheduler"
+scheduler.cron '0 6 * * 1 *' do
+  puts "Updating all the players..."
+  Player.update_players
 end
